c61a3251779a41a804aebeaba0b7c2a6636163c9
Rukash <rafael.ieel@hotmail.com>
2020-10-12 17:05:56 -0300

    Landing Page and configs done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import './styles/global.css'
+import Routes from './routes'
 
 function App () {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <Routes />
   )
 }
 
---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/pages/landing.css'
+import Logo from '../images/logo.svg'
+import { Link } from 'react-router-dom'
+import { FiArrowRight } from 'react-icons/fi'
+
+function Landing () {
+  return (
+    <div id='page-landing'>
+      <div className='content-wrapper'>
+        <img src={Logo} alt='Logo imagem' />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className='location'>
+          <strong>Florianopólis</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <Link to='/app' className='enter-app'>
+          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
