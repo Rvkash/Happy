886f88cb330b1178204d2ddbf3122a1d3944cb57
Rukash <rafael.ieel@hotmail.com>
2020-10-13 19:05:30 -0300

    add models config errors and image views

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import './database/connection'
+import path from 'path'
+import errorHandler from './errors/handler'
+
+import routes from './routes'
 
 const app = express()
 
-app.get('/', function (req, res) {
-  res.json({ message: 'hello world' })
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
