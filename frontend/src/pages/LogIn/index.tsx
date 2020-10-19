9012459ec4b4e7bc93f83f04cd1a034365ed5e24
Rukash <rafael.ieel@hotmail.com>
2020-10-19 18:15:21 -0300

    add register page

--- frontend/src/pages/LogIn/index.tsx
+++ frontend/src/pages/LogIn/index.tsx
@@ -1,33 +1,80 @@
-import React from 'react'
-import './styles.css'
+import React, { FormEvent, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar'
 
-function Dashboard () {
+import  './styles.css'
+import api from '../../services/api'
+
+
+export default function LogIn () {
+  const history = useHistory()
+
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    console.log({name, email, senha})
+    const data = new FormData()
+    
+    data.append('name',  name)
+    data.append('email', email)
+    data.append('senha', senha)
+
+    try {
+     await api.post('/users', data)
+      alert('Cadastro realizado')
+
+      history.push('users')
+    }catch(err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div id='page-create-orphanage'>
       <Sidebar />
-
       <main>
-        <form className='create-orphanage-form'>
-          <fieldset>
-            <legend>Cadastro Perfil</legend>
-            <div className='input-block'>
-              <label htmlFor='name'>Nome</label>
-              <input id='name' />
-              <label htmlFor='email'>Email</label>
-              <input id='email' />
+        <form onSubmit={handleSubmit} className='create-orphanage-form'>
+            <fieldset>
+              <legend>Cadastro Perfil</legend>
+              <div className='input-block'>
+                <label htmlFor='name'>Nome</label>
+                <input 
+                id='name' 
+                value={name} 
+                onChange={e => setName(e.target.value)} 
+              />
+
+              <div className='input-block'> 
+                <label htmlFor='email'>Email</label>
+                <input 
+                id='name' 
+                value={email} 
+                onChange={e => setEmail(e.target.value)} 
+              />
+
               <label htmlFor='senha'>Senha</label>
-              <input id='senha' />
-              <button className='confirm-button' type='submit'>
-               Confirmar
-              </button>
-              <a href='/'>Já tenho uma conta</a>
-            </div>
-          </fieldset>
+                <input 
+                id='name' 
+                value={senha} 
+                onChange={e => setSenha(e.target.value)} 
+                />
+              </div>
+          
+              </div>
+            </fieldset>
+
+            <button className='confirm-button' type='submit'>
+              Confirmar
+            </button>
+
+            <a href='/'>Já tenho uma conta</a>
         </form>
       </main>
     </div>
   )
 }
-
-export default Dashboard
