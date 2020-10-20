ba539e139befe8c055829bea916f94f87d751840
Rukash <rafael.ieel@hotmail.com>
2020-10-20 01:30:29 -0300

    fix: api post, 201 OK

--- frontend/src/pages/LogIn/index.tsx
+++ frontend/src/pages/LogIn/index.tsx
@@ -1,35 +1,31 @@
-import React, { FormEvent, useState } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar'
 
-import  './styles.css'
+import './styles.css'
 import api from '../../services/api'
-
 
 export default function LogIn () {
   const history = useHistory()
 
-  
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
+  const [password, setPassword] = useState('')
 
-  async function handleSubmit(e: FormEvent) {
+  async function handleSubmit (e: any) {
     e.preventDefault()
 
-    console.log({name, email, senha})
-    const data = new FormData()
-    
-    data.append('name',  name)
-    data.append('email', email)
-    data.append('senha', senha)
+    const data = {
+      name,
+      email,
+      password
+    }
 
     try {
-     await api.post('/users', data)
-      alert('Cadastro realizado')
+      await api.post('/users', data)
 
-      history.push('users')
-    }catch(err) {
+      history.push('/login')
+    } catch (err) {
       console.error(err)
     }
   }
@@ -37,42 +33,45 @@ export default function LogIn () {
   return (
     <div id='page-create-orphanage'>
       <Sidebar />
+
       <main>
         <form onSubmit={handleSubmit} className='create-orphanage-form'>
-            <fieldset>
-              <legend>Cadastro Perfil</legend>
-              <div className='input-block'>
-                <label htmlFor='name'>Nome</label>
-                <input 
-                id='name' 
-                value={name} 
-                onChange={e => setName(e.target.value)} 
-              />
+          <fieldset>
+            <legend>Faça seu registro</legend>
 
-              <div className='input-block'> 
-                <label htmlFor='email'>Email</label>
-                <input 
-                id='name' 
-                value={email} 
-                onChange={e => setEmail(e.target.value)} 
+            <div className='input-block'>
+              <label htmlFor='name'>Nome</label>
+              <input
+                id='name'
+                value={name}
+                onChange={e => setName(e.target.value)}
               />
+            </div>
 
+            <div className='input-block'>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='name'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className='input-block'>
               <label htmlFor='senha'>Senha</label>
-                <input 
-                id='name' 
-                value={senha} 
-                onChange={e => setSenha(e.target.value)} 
-                />
-              </div>
-          
-              </div>
-            </fieldset>
+              <input
+                type='password'
+                id='name'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
 
-            <button className='confirm-button' type='submit'>
-              Confirmar
-            </button>
-
-            <a href='/'>Já tenho uma conta</a>
+          </fieldset>
+          <button className='confirm-button' type='submit'>
+            Confirmar
+          </button>
         </form>
       </main>
     </div>
