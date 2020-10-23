aec0ad0e6d0d9edb479c77b8451942edc9954236
Rukash <rafael.ieel@hotmail.com>
2020-10-23 17:56:17 -0300

    user models

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -14,25 +14,5 @@ export default class User {
 
 	@Column()
 	password: string;
-
-	UserSchema.pre("save", async function hashPassword(next) {
-  if (!this.isModified("password")) next();
-
-  this.password = await bcrypt.hash(this.password, 8);
-});
-
-UserSchema.methods = {
-  compareHash(hash) {
-    return bcrypt.compare(hash, this.password);
-  },
-
-  generateToken() {
-    return jwt.sign({ id: this.id }, "secret", {
-      expiresIn: 86400
-    });
-  }
-};
-
-
 }
 
